7b56ccd4b3f6c6ca3a48dcfeadc276a4350677a4
Griotold <gotjd9773@naver.com>
2024-12-23 21:01:03 +0900

    [common] AOP - 권한 체크, 컨트롤러 진입시 사용자 요청 로그 찍기

--- common/src/main/java/com/griotold/exception/ErrorCode.java
+++ common/src/main/java/com/griotold/exception/ErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "접근 권한이 존재하지 않습니다."),
+    MISSING_ROLE(HttpStatus.BAD_REQUEST, "권한 정보가 없습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "유저 ID 또는 비밀번호 정보가 일치하지 않습니다."),
 
     ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "일치하는 엔티티가 존재하지 않습니다."),
--- product/src/main/java/com/griotold/product/presentation/controller/ProductController.java
+++ product/src/main/java/com/griotold/product/presentation/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.griotold.product.presentation.controller;
 
+import com.griotold.infra.aspect.RequireRole;
 import com.griotold.presentation.ApiResponse;
 import com.griotold.product.application.dto.ProductResponse;
 import com.griotold.product.application.service.ProductService;
@@ -16,11 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/products")
 @RestController
 public class ProductController {
+
     private final ProductService productService;
 
     @PostMapping
     public ApiResponse<ProductResponse> createProduct(@RequestBody ProductCreateRequest productCreateRequest) {
-        log.info("createProduct.ProductCreateRequest {}", productCreateRequest);
         ProductResponse response = productService.createProduct(productCreateRequest.toServiceDto());
         return ApiResponse.success(response);
     }
